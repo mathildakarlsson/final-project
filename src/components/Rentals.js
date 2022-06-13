fdb0a1fbd78728aa6423a8b5ad30c83ee557d8f8
mathildakarlsson <86979661+mathildakarlsson@users.noreply.github.com>
2022-06-13 12:32:25 +0200

    removed typos

--- src/components/Rentals.js
+++ src/components/Rentals.js
@@ -26,7 +26,7 @@ const AddButton = styled.button`
     background-color: black;
     color: white;
     transition: 0.7s ease;
-    heigth: 30px;
+    height: 30px;
     }
 `;
 
--- src/components/SingleRentalsItem.js
+++ src/components/SingleRentalsItem.js
@@ -24,7 +24,7 @@ const AddButton = styled.button`
     background-color: black;
     color: white;
     transition: 0.7s ease;
-    heigth: 30px;
+    height: 30px;
     }
 `;
 
