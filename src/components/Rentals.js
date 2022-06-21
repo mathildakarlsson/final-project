fbf6c4a9ac51a521f7d559bf3d2ab3b400e147e3
Louisa <lundislon@gmail.com>
2022-06-22 00:25:01 +0200

    added styling to Rentals

--- src/components/Rentals.js
+++ src/components/Rentals.js
@@ -41,24 +41,15 @@ const Rentals = () => {
     }, []);
 
     return (
-        <main>
-            <h2>Welcome to the rentals web shop!</h2>
-            {/* <section className="wishlist">
-                <h2>Din varukorg</h2>
-                <ul className="wishlist-ul">
-                    {products.map((product, index) => {
-                        return(
-                            <WishListItem key={index} product={product} />
-                        )
-                    })}
-                </ul>
-                <p className="total">Total cost: {totalPrice} SEK</p>
-            </section> */}
-            <section className="product-container">
-                <div className="product-grid">
-
+        <Main>
+            <ArticleContainer>
+                <Header>Hyrsaker</Header>
+                <Info>Yes! Dags att välja dekoration!
+                    Lägg sådant du önskar i din önskelista och skicka den sen till oss så återkommer vi med
+                    offert för dig att ta ställning till.</Info>
+                    <RentalsContainer>
                     {productData && productData.map((product, index) => (
-                        <article className="product-card" key={index}>
+                        <CardContainer key={index}>
                             <Link to={"/product/" + product.slug.current} key={product.slug.current}>
                                 <span className="product-span" key={index}>
                                     <img className="product-image"
@@ -66,9 +57,9 @@ const Rentals = () => {
                                         alt={product.title}
                                     />
                                      <span className="title-span" key={index}> 
-                                        <h3 className="product-title">{product.title}</h3>
+                                        <RentalsInfoText>{product.title}</RentalsInfoText>
                                      </span>
-                                     <p>{product.defaultProductVariant.price} {product.defaultProductVariant.sku}/day</p>
+                                     <RentalsInfoText>{product.defaultProductVariant.price} {product.defaultProductVariant.sku}/dag</RentalsInfoText>
                                 </span>
                             </Link> 
                             <AddButton
@@ -76,24 +67,107 @@ const Rentals = () => {
                                 // type="button"
                                 disabled={product.inventory === 0}
                                 onClick={() => dispatch(wishlist.actions.addItem(product))}>
-                                Add to wishlist
+                                Lägg i önskelistan
                             </AddButton>
-                        </article> 
+                        </CardContainer> 
                     ))}
-                    
-                </div>
-            </section>  
-        </main>
+                    </RentalsContainer>
+            </ArticleContainer>
+        </Main>
     )
 };
 
 export default Rentals;
 
 
+const Main = styled.main`
+    display: flex;
+    flex-direction: column;
+    margin: 1rem;
+`
+
+const ArticleContainer = styled.article`
+    padding: 8rem 2rem 0 2rem;
+    margin: 0px 30px;
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    align-self: center;
+    justify-self: center;
+`
+
+const Header = styled.h1 `
+    text-transform: uppercase;
+    font-weight: 300;
+    padding-bottom: 2.5rem;
+    font-size: 32px;
+    letter-spacing: .15em;
+
+    @media (min-width: 768px) {
+        font-size: 40px;
+        letter-spacing: .2em;
+    }
+
+`
+
+const Info = styled.h4 `
+    font-weight: 300;
+    margin: 12px 0;
+    line-height: 28px;
+    padding-bottom: 1rem;
+
+    @media (min-width: 768px) {
+        font-size: 20px;
+        letter-spacing: .03em;
+    }
+`
+
+const RentalsContainer = styled.div`
+@media (min-width: 768px) {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr)
+}
+
+@media (min-width: 1024px) {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr)
+}
+`
+
+const RentalsInfoText = styled.h1 `
+    text-transform: uppercase;
+    font-weight: 300;
+    font-size: 20px;
+    letter-spacing: .15em;
+    background-color: white;
+
+    @media (min-width: 668px) {
+        font-size: 15px;
+        letter-spacing: .2em;
+    }
+
+`
+
+const CardContainer = styled.div`
+background-color: white;
+width: 55vh;
+padding: 1rem 1 rem 1 rem 0;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+margin: 1.5rem;
+
+@media (min-width: 668px) {
+    width: 35vh;
+    padding: o.5rem 0.5 rem 0.5rem 0;
+
+}
+`
 
 const AddButton = styled.button`
-    width: 55%;
-    margin: 15px 0 65px 0;
+    width: 100%;
+    margin: 15px 0 0 0;
     cursor: pointer;
     border: none;
     font-size: 16px;
@@ -124,3 +198,15 @@ const AddButton = styled.button`
             <product key={index} product={index} /> //ändrat till index och _id här
         ))}
      </div> */}
+
+       {/* <section className="wishlist">
+                <h2>Din varukorg</h2>
+                <ul className="wishlist-ul">
+                    {products.map((product, index) => {
+                        return(
+                            <WishListItem key={index} product={product} />
+                        )
+                    })}
+                </ul>
+                <p className="total">Total cost: {totalPrice} SEK</p>
+            </section> */}
